43b13e5c26fed9fa01b8ed2c5b0a62ec9be8e85b
Mark Fine <mark.fine@gmail.com>
2014-10-20 23:02:35 -0700

    kill the snapshot.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject fine.core "1.0.0-SNAPSHOT"
+(defproject fine.core "1.0.0"
   :description "Fine's core libraries"
   :url "http://github.com/mfine/fine.core"
+  :author "mfine"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
