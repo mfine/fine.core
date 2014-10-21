0d921c294bb3dd9de404e69f0686b8c440ca9241
Mark Fine <mark.fine@gmail.com>
2014-10-20 23:14:48 -0700

    sign releases.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :deploy-repositories [["clojars" {:sign-releases false}]])
+  :deploy-repositories [["clojars"]])
